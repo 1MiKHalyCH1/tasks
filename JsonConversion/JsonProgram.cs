3204ba79c2e053718397702d5af04ec5e07c0955
SokolovArtur <admin@saytstroy.ru>
2017-04-09 10:46:31 +0300

    tests

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -5,26 +5,32 @@ using System.Linq;
 
 namespace JsonConversion
 {
-	public class JsonProgram
+	public static class JsonProgram
 	{
 		static void Main()
 		{
             string json = Console.In.ReadToEnd();
             //var json = "{\"version\":\"2\",\"products\":{\"1\":{\"name\":\"Pen\",\"price\":12,\"count\":100},\"2\":{\"name\":\"Pencil\",\"price\":8,\"count\":1000},\"3\":{\"name\":\"Box\",\"price\":12.1,\"count\":50}}}";
-            JObject v2 = JObject.Parse(json);
-		    var products = v2["products"].ToObject<Dictionary<string, JObject>>();
-            var _products = new List<JObject>();
-		    foreach (var key in products.Keys)
-		    {
-		        var obj = products[key];
-		        obj.Add("id", int.Parse(key));
-		        _products.Add(obj);
-		    }
-            var bv3 = new JObject(
-                new JProperty("version", "3"),
-                new JProperty("products",_products)
-                );
-			Console.Write(bv3);
+            var bv3 = ConverJson(json);
+		    Console.Write(bv3);
 		}
+
+	    public static JObject ConverJson(string json)
+	    {
+	        JObject v2 = JObject.Parse(json);
+	        var products = v2["products"].ToObject<Dictionary<string, JObject>>();
+	        var _products = new List<JObject>();
+	        foreach (var key in products.Keys)
+	        {
+	            var obj = products[key];
+	            obj.Add("id", int.Parse(key));
+	            _products.Add(obj);
+	        }
+	        var bv3 = new JObject(
+	            new JProperty("version", "3"),
+	            new JProperty("products", _products)
+	        );
+	        return bv3;
+	    }
 	}
 }
---
+++ JsonConversion/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace JsonConversion
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase("1.v2.json", "1.v3.json")]
+        [TestCase("2.v2.json", "2.v3.json")]
+        public void JsonTestV1(string v2, string v3)
+        {
+            var jsonV2 = "";
+            using (StreamReader sr = new StreamReader(v2))
+            {
+                jsonV2 = sr.ReadToEnd();
+            }
+            JObject jsonV3 = new JObject();
+            using (StreamReader sr = new StreamReader(v3))
+            {
+                jsonV3 = JObject.Parse(sr.ReadToEnd());
+            }
+
+            JObject resultJson = JsonProgram.ConverJson(jsonV2);
+            Assert.AreEqual(jsonV3, resultJson);
+        }
+    }
+}
