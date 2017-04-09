0d2c34917608d6fcab6d744b5b4139927cc8591c
hommiusx <hommiusx@gmail.com>
2017-04-09 14:27:15 +0500

    Eval2 1try

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -1,28 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ExpressionEvaluator;
+using Newtonsoft.Json.Linq;
 
 namespace EvalTask
 {
-	class EvalProgram
-	{
-		static void Main(string[] args)
-		{
-            //string input = Console.In.ReadToEnd();
-		    string expresionInput = Console.ReadLine();
-		    expresionInput = expresionInput.Replace("-", " - ");
+    public class EvalProgram
+    {
 
-		    var variables = expresionInput.Split(new char[] {' ', '+', '-', '*', '/', '(', ')', '{', '}',',','.'}, StringSplitOptions.RemoveEmptyEntries);
+        public static string[] GetVariablesFromExpression(string expression)
+        {
+            return expression.Split(new char[] {' ', '+', '-', '*', '/', '(', ')', '{', '}', ',', '.','1','2','3','4','5','6','7','8','9','0'},
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .ToArray();
+        }
 
-		  //  var json = Console.In.ReadToEnd();
-		    var json = @"{
-		    a: 5
-		}";
-		    foreach (var result in SimQLProgram.SimQLProgram.ExecuteQueries(json))
-		        Console.WriteLine(result);
 
-          //  var expresion = new CompiledExpression(expresionInput);
-		 //   var output = expresion.Eval();
-		//	Console.WriteLine(output);
-		}
-	}
+
+        public static string EvaluateStringExpression(string expression)
+        {
+            expression = expression.Replace("-", " - ");
+            var compiledExpression = new CompiledExpression(expression);
+            var result = compiledExpression.Eval();
+            return result.ToString();
+        }
+
+
+        public static string EvaluateStringExpression(string expression, Dictionary<string, string> variablesDictionary)
+        {
+            expression = expression.Replace("-", " - ");
+            foreach (var v in variablesDictionary.OrderByDescending(x => x.Key.Length))
+            {
+                expression = expression.Replace(v.Key, v.Value);
+            }
+            var compiledExpression = new CompiledExpression(expression);
+            var result = compiledExpression.Eval();
+            return result.ToString();
+        }
+
+        public static Dictionary<string, string> GetDictionaryWihVariablesFromJson(JObject json, string[] variables)
+        {
+            var jObject = new JObject(new JProperty("data", json));
+            var dictionary = new Dictionary<string, string>();
+
+            foreach (var variable in variables)
+            {
+                var value = SimQLProgram.SimQLProgram.ExecuteQuery(jObject, "data", variable);
+                var temp = value.Split(new string[] { " = " }, StringSplitOptions.RemoveEmptyEntries);
+                dictionary.Add(temp[0], temp[1]);
+            }
+            return dictionary;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = Console.In.ReadToEnd().Split('\n');
+
+            string expression = input[0];
+            var jsonInput = String.Join("\n", input.Skip(1).ToArray());
+            if (jsonInput == "")
+                jsonInput = "{}";
+            //var jsonInput = "{'a':50, 'b':45}";
+            var jObject =  JObject.Parse(jsonInput);
+            var variables = GetVariablesFromExpression(expression);
+            var dictionary = GetDictionaryWihVariablesFromJson(jObject, variables);
+            var result = EvaluateStringExpression(expression,dictionary);
+            Console.WriteLine(result);
+        }
+    }
 }
