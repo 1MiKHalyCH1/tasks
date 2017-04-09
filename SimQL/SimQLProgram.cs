451364bea4a030583ef4fff247e4f74b577ad0de
1MiKHalyCH1 <bykoun@mail.ru>
2017-04-09 12:16:44 +0600

    ExecuteQuery signature fix

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -11,7 +11,8 @@ namespace SimQLTask
 	{
 		static void Main(string[] args)
 		{
-            var json = Console.In.ReadToEnd();
+            //var json = Console.In.ReadToEnd();
+		    var json = "{'data':{'a':{'x':3.14,'b':{'c':15},'c':{'c':9}},'z':42},'queries':['a.b.c','z','a.x']}";
             foreach (var result in ExecuteQueries(json))
 				Console.WriteLine(result);
 		}
@@ -19,15 +20,15 @@ namespace SimQLTask
 		public static IEnumerable<string> ExecuteQueries(string json)
 		{
 			var jObject = JObject.Parse(json);
-			var data = (JObject)jObject["data"];
 			var queries = jObject["queries"].ToObject<string[]>();
 
 		    return queries
-		            .Select(q => ExecuteQuery(data, q));
+		            .Select(q => ExecuteQuery(jObject, "data", q));
 		}
 
-        private static string ExecuteQuery(JObject data, string query)
+        public static string ExecuteQuery(JObject mainObject, string rootName , string query)
         {
+            var data = mainObject[rootName];
             try
             {
                 var token = data.SelectToken(query).ToString();
